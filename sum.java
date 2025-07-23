111202a7ccb00ee928045151801e926f003d8425
Sathvika1110 <245122748009@mvsrec.edu.in>
2025-07-23 10:07:22 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,15 @@
+class sum
+{
+     public static void main(String args[])
+     {
+           int sum=0;
+           for(i=1;i<=10;i++)
+           {
+                 sum+=i;
+           }
+           System.out.println("sum:"+sum);
+           float avg=sum/n
+           System.out.println("Avg:"+avg);
+     }
+}
+
