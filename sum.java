09a41eca743b7a18e3bcae2fb66a5bfd9727afdd
Sathvika1110 <245122748009@mvsrec.edu.in>
2025-07-23 10:12:46 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,15 +1,13 @@
-class sum
-{
-     public static void main(String args[])
-     {
-           int sum=0;
-           for(i=1;i<=10;i++)
-           {
-                 sum+=i;
-           }
-           System.out.println("sum:"+sum);
-           float avg=sum/n
-           System.out.println("Avg:"+avg);
-     }
+class Sum {
+    public static void main(String args[]) {
+        int sum = 0;
+        int n = 10; // Total numbers from 1 to 10
+        for (int i = 1; i <= n; i++) { // Declare i as int
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+        float avg = (float) sum / n; // Cast sum to float for accurate division
+        System.out.println("Avg: " + avg);
+    }
 }
 
